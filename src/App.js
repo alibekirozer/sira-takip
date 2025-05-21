5d82215c67c550e2c7e29831e9e5cb53448b04fd
alibekirozer <muhammedalibekir@gmail.com>
2025-05-22 00:13:33 +0300

    4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { signOut } from "firebase/auth";
 import { auth } from "./firebase";
 import AdminPanel from "./AdminPanel";
 import { update } from "firebase/database";
-import { sendTeamsNotification } from './utils/sendTeamsNotification';
+import { sendTeamsNotification } from './sendTeamsNotification';
 
 export default function SiraTakip() {
   const [allEmployees, setAllEmployees] = useState([]);
@@ -59,9 +59,7 @@ export default function SiraTakip() {
     const index = siradakiIndex();
     const siradaki = activeList[index]?.name;
     if (siradaki && siradaki === benimAdim) bildirimGonder(benimAdim);
-    if (siradaki) {
       sendTeamsNotification(`Şu an çağrı sırası ${siradaki}'de!`);
-    }
   }, [currentIndex, activeList, benimAdim]);
 
   useEffect(() => {
