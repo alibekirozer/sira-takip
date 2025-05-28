ad09456e312562e5d0dca0a4808561a974164833
Ali Bekir Özer <82832257+alibekirozer@users.noreply.github.com>
2025-05-28 16:39:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -357,7 +357,7 @@ export default function SiraTakip() {
           {/* Sağ tarafta bilgi kısmı */}
           <div className="flex-1 flex justify-center">
             <div className="bg-blue-100 dark:bg-blue-900 text-blue-900 dark:text-blue-100 rounded px-[1vw] py-[0.6vh] text-[clamp(0.7rem,1vw,1.1rem)] font-medium shadow-sm text-center whitespace-nowrap">
-              Sıra şimdi <span className="font-bold">{siradakiKisi()}</span>'de/da, size sıra gelmesine <span className="font-bold">{kalanKisiSayisi()}</span> kişi var.
+              Sıradaki kişi: <span className="font-bold">{siradakiKisi()}</span>. Size sıra gelmesine <span className="font-bold">{kalanKisiSayisi()}</span> kişi var.
             </div>
           </div>
         </div>
