dff99a92399c63ec5d4c2a369b475d439b97207b
Ali Bekir Özer <82832257+alibekirozer@users.noreply.github.com>
2025-07-03 15:53:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -439,7 +439,6 @@ export default function SiraTakip() {
                   "flex flex-col gap-[0.4vh] rounded-[0.4vw] shadow-sm p-[0.6vw] duration-200",
                   "bg-white",
                   i === siradakiMusaitIndex() && "bg-blue-50 border-2 border-green-500",
-                  isCurrentUser && "ring-2 ring-blue-50 bg-blue-50 dark:bg-blue-950"
                 )}
               >
                 {/* Üst Satır: Durum rengi ve isim */}
